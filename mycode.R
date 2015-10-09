0481edecd3fd9f8955d3221ce481d1dd10d9987b
johngonz <johngonz@gmail.com>
2015-10-09 11:29:47 -0400

    added programming assignment 2

--- mycode.R
+++ mycode.R
@@ -31,4 +31,50 @@ columnmean <- function(y, removeNA=TRUE){
          dat_subset <- dat[which(dat[, "Day"] == day),]  #subsets the rows that match the 'day' argument
          median(dat_subset[, "Weight"], na.rm=TRUE)      #identifies the median weight 
          #while stripping out the NAs
+ }
+
+ 
+ makeVector <- function(x = numeric()) {
+     m <- NULL
+     set <- function(y) {
+         x <<- y
+         m <<- NULL
+     }
+     get <- function() x
+     setmean <- function(mean) m <<- mean
+     getmean <- function() m
+     list(set = set, get = get,
+          setmean = setmean,
+          getmean = getmean)
+ }
+ cachemean <- function(x, ...) {
+     m <- x$getmean()
+     if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+     }
+     data <- x$get()
+     m <- mean(data, ...)
+     x$setmean(m)
+     m
+ }
+ 
+ open.account <- function(total) {
+     list(
+         deposit = function(amount) {
+             if(amount <= 0)
+                 stop("Deposits must be positive!\n")
+             total <<- total + amount
+             cat(amount, "deposited.  Your balance is", total, "\n\n")
+         },
+         withdraw = function(amount) {
+             if(amount > total)
+                 stop("You don't have that much money!\n")
+             total <<- total - amount
+             cat(amount, "withdrawn.  Your balance is", total, "\n\n")
+         },
+         balance = function() {
+             cat("Your balance is", total, "\n\n")
          }
+     )
+ }
